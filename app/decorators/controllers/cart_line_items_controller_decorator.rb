9fb83dfcca810e12d8eb6dbb99bd1cfd6cf74c69
Mayur Shah <shahmayur001@gmail.com>
2025-03-05 02:57:38 +0530

    Store customer metadata with cart line items
    - Allow capturing serial numbers/IMEI via customer_metadata
    - Generate unique keys for metadata entries
    - Add validation for reasonable quantity ranges

---
+++ app/decorators/controllers/cart_line_items_controller_decorator.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+module Spree
+  module Api
+    module CartLineItemsControllerDecorator
+      def create
+        @order = current_order(create_order_if_necessary: true)
+        authorize! :update, @order, cookies.signed[:guest_token]
+
+        variant  = Spree::Variant.find(params[:variant_id])
+        quantity = params[:quantity].present? ? params[:quantity].to_i : 1
+
+        customer_metadata = params.require(:customer_metadata).permit! if params[:customer_metadata].present?
+        # 2,147,483,647 is crazy. See issue https://github.com/spree/spree/issues/2695.
+        if quantity.between?(1, 2_147_483_647)
+          begin
+            @line_item = @order.contents.add(variant, quantity)
+            if customer_metadata.present?
+              customer_metadata.each do |key, value|
+                unique_key = "#{key}_#{SecureRandom.hex(4)}"
+                @line_item.customer_metadata[unique_key] = value
+              end
+              @line_item.save
+            end
+          rescue ActiveRecord::RecordInvalid => e
+            @order.errors.add(:base, e.record.errors.full_messages.join(", "))
+          end
+        else
+          @order.errors.add(:base, t('spree.please_enter_reasonable_quantity'))
+        end
+
+        respond_to do |format|
+          format.html do
+            if @order.errors.any?
+              flash[:error] = @order.errors.full_messages.join(", ")
+              redirect_back_or_default(root_path)
+              return
+            else
+              redirect_to cart_path
+            end
+          end
+        end
+      end
+    end
+  end
+end
+
+CartLineItemsController.prepend(Spree::Api::CartLineItemsControllerDecorator)
