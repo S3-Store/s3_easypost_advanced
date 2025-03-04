3a1efc512555e4fa4b750523b1e7487860c95386
Mayur Shah <shahmayur001@gmail.com>
2025-03-05 02:57:52 +0530

    Improve shipping calculator with full dimensional support
    - Consider height/width/depth in addition to weight
    - Calculate max dimensions for accurate rate estimates

--- lib/solidus_easypost/calculator/weight_dimension_calculator.rb
+++ lib/solidus_easypost/calculator/weight_dimension_calculator.rb
@@ -11,11 +11,25 @@ module SolidusEasypost
       end
 
       def compute_for_package(package)
-        total_weight = package.contents.sum do |item|
-          item.quantity * item.variant.weight
+        total_weight = 0.0
+        total_height = 0.0
+        max_width = 0.0
+        max_depth = 0.0
+
+        package.contents.each do |item|
+          variant = item.variant
+          total_weight += variant.weight * item.quantity
+          total_height += variant.height * item.quantity
+          max_width = [max_width, variant.width].max
+          max_depth = [max_depth, variant.depth].max
         end
 
-        SolidusEasypost::ParcelDimension.new(weight: total_weight)
+        SolidusEasypost::ParcelDimension.new(
+          weight: total_weight,
+          height: total_height,
+          width: max_width,
+          depth: max_depth
+        )
       end
     end
   end
